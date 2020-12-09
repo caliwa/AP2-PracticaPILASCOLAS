51edf8ad88d13befeddb38afb27bddd367702283
AyPII <santiago_olmedo82201@elpoli.edu.co>
2020-12-08 22:55:43 -0500

    Punto 3

---
+++ Punto 3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "MetodosCola.h"
+#include <conio.h>
+using namespace std;
+int main(int argc, char** argv) {
+	int i=10;
+	Cola c1 (i);
+	c1.LlenarDesdeLista(); 
+	cout<<"************* SE INSERTO LA INFORMACION A LA COLA *************"<<endl;
+	cout<<"*********************************************"<<endl;
+	while (!i==0){
+		c1.MostrarCola(i);
+		i--;
+	};
+	return 0;
+}
